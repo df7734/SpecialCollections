a668ba73cb83af165c842903dd0745a5ec9b8e62
df7734 <df7734@gmail.com>
2021-12-21 21:18:55 +0200

    1

--- src/main/java/com/efimchick/ifmo/collections/MedianQueue.java
+++ src/main/java/com/efimchick/ifmo/collections/MedianQueue.java
@@ -1,5 +1,88 @@
 package com.efimchick.ifmo.collections;
 
-class MedianQueue  {
 
+import java.util.*;
+
+class MedianQueue extends PriorityQueue {
+    private ArrayList<Integer> arrayList = new ArrayList<>();;
+
+    @Override
+    public boolean offer(Object o) {
+        System.out.println(o);
+        return arrayList.add((Integer) o);
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return arrayList.iterator();
+    }
+
+    public ArrayList<Integer> sort(ArrayList<Integer> a){
+        Collections.sort(a);
+        //swap(0, (arrayList.size() - 1)/2, a);
+        ArrayList<Integer> finalArray = new ArrayList<>();
+        if(a.size() == 2){
+            finalArray.add(a.get(0));
+            finalArray.add(a.get(1));
+        }
+        if(a.size() == 3){
+            finalArray.add(a.get((arrayList.size() - 1)/2));
+            finalArray.add(a.get((arrayList.size() - 1)/2 - 1));
+            finalArray.add(a.get((arrayList.size() - 1)/2 + 1));
+
+        }
+        if(a.size() == 5){
+                finalArray.add(a.get((arrayList.size() - 1)/2));
+                finalArray.add(a.get((arrayList.size() - 1)/2 - 1));
+                finalArray.add(a.get((arrayList.size() - 1)/2 + 1));
+                finalArray.add(a.get((arrayList.size() - 1)/2 - 2));
+                finalArray.add(a.get((arrayList.size() - 1)/2 + 2));
+        }
+        if(a.size() == 9){
+            finalArray.add(a.get((arrayList.size() - 1)/2));
+            finalArray.add(a.get((arrayList.size() - 1)/2 - 1));
+            finalArray.add(a.get((arrayList.size() - 1)/2 + 1));
+            finalArray.add(a.get((arrayList.size() - 1)/2 - 2));
+            finalArray.add(a.get((arrayList.size() - 1)/2 + 2));
+            finalArray.add(a.get((arrayList.size() - 1)/2 - 3));
+            finalArray.add(a.get((arrayList.size() - 1)/2 + 3));
+            finalArray.add(a.get((arrayList.size() - 1)/2 - 4));
+            finalArray.add(a.get((arrayList.size() - 1)/2 + 4));
+        }
+        if(a.size() == 10){
+            finalArray.add(a.get((arrayList.size() - 1)/2));
+            finalArray.add(a.get((arrayList.size() - 1)/2 + 1));
+            finalArray.add(a.get((arrayList.size() - 1)/2 - 1));
+            finalArray.add(a.get((arrayList.size() - 1)/2 + 2));
+            finalArray.add(a.get((arrayList.size() - 1)/2 - 2));
+            finalArray.add(a.get((arrayList.size() - 1)/2 + 3));
+            finalArray.add(a.get((arrayList.size() - 1)/2 - 3));
+            finalArray.add(a.get((arrayList.size() - 1)/2 + 4));
+            finalArray.add(a.get((arrayList.size() - 1)/2 - 4));
+            finalArray.add(a.get((arrayList.size() - 1)/2 + 5));
+        }
+        return finalArray;
+    }
+    void swap(int x, int y, List <Integer> myList) {
+        Integer i = myList.get(x);
+        myList.set(x, myList.get(y));
+        myList.set(y, i);
+    }
+
+    @Override
+    public int size() {
+        return arrayList.size();
+    }
+
+    @Override
+    public Integer peek() {
+        arrayList = sort(arrayList);
+
+        return arrayList.get(0);
+    }
+
+    @Override
+    public Object poll() {
+        return arrayList.remove(0);
+    }
 }
--- src/main/java/com/efimchick/ifmo/collections/PairStringList.java
+++ src/main/java/com/efimchick/ifmo/collections/PairStringList.java
@@ -1,5 +1,67 @@
 package com.efimchick.ifmo.collections;
 
-class PairStringList  {
 
+import java.util.*;
+
+class PairStringList extends ArrayList {
+    @Override
+    public boolean add(Object o) {
+        super.add(o);
+        return super.add(o);
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        super.remove(o);
+        return super.remove(o);
+    }
+
+    @Override
+    public Object remove(int index) {
+            super.remove(index);
+            return super.remove(index-1);
+    }
+
+    @Override
+    public void add(int index, Object element) {
+        if(index%2!=0){
+            index++;
+        }
+        super.add(index, element);
+        super.add(index, element);
+    }
+
+    @Override
+    public Object set(int index, Object element) {
+        if(index%2!=0){
+            index--;
+        }
+        super.set(index, element);
+        return super.set(index+1, element);
+    }
+
+    @Override
+    public boolean addAll(Collection c) {
+        List<Object> objectList = Arrays.asList(c.toArray());
+        Collection<Object> collection = new ArrayList<>();
+        for (Object o: objectList) {
+            collection.add(o);
+            collection.add(o);
+        }
+        return super.addAll(collection);
+    }
+
+    @Override
+    public boolean addAll(int index, Collection c) {
+        if(index%2!=0){
+            index++;
+        }
+        List<Object> objectList = Arrays.asList(c.toArray());
+        Collection<Object> collection = new ArrayList<>();
+        for (Object o: objectList) {
+            collection.add(o);
+            collection.add(o);
+        }
+        return super.addAll(index, collection);
+    }
 }
